30c77a0904555e96e9a85d0736ce23c0ca9ec8f1
unknown <873025019@qq.com>
2020-12-30 21:14:17 +0800

    第一次commit

---
+++ src/components/characters/Characteritem.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const CharacterItem = ({ item }) => {
+  return (
+    <div className="card">
+      <div className="card-inner">
+        <div className="card-front">
+          <img src={item.img} alt="" />
+        </div>
+        <div className="card-back">
+          <h1>{item.name}</h1>
+          <ul>
+            <li>
+              <strong>演员名字:</strong> {item.portrayed}
+            </li>
+            <li>
+              <strong>昵称:</strong> {item.nickname}
+            </li>
+            <li>
+              <strong>职业:</strong> {item.occupation}
+            </li>
+            <li>
+              <strong>生日:</strong> {item.birthday}
+            </li>
+            <li>
+              <strong>目前状况:</strong> {item.status}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CharacterItem;
